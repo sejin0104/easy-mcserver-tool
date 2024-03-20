d39bbfed4874b90ee5ed4a2950ccb5d8cc3588f0
sejin0104 <sejinjin1101@gmail.com>
2024-03-20 22:39:30 +0900

    chanage info_ip

--- info_ip.py
+++ info_ip.py
@@ -1,45 +1,34 @@
 import requests
 import json
 
-def get_ip_info(ip_version='ipv4', output_format='json'):
-    base_url = 'https://api.ip.pe.kr/'
-
-    if ip_version == 'ipv4':
+def get_ip_info(output_format='json'):
+    try:
+        base_url = 'https://api.ip.pe.kr/'
         endpoint = base_url + 'json/' if output_format == 'json' else base_url + 'xml/'
-    elif ip_version == 'ipv6':
-        endpoint = base_url + 'json/' if output_format == 'json' else base_url + 'xml/'
-    else:
-        print("Invalid IP version specified.")
-        return
 
-    response = requests.get(endpoint)
+        response = requests.get(endpoint)
     
-    if response.status_code == 200:
-        data = response.json() if output_format == 'json' else response.text
-        return data
-    else:
-        print("Failed to fetch IP information.")
-        return None
+        if response.status_code == 200:
+            data = response.json() if output_format == 'json' else response.text
+            return data
+        else:
+            print("Failed to fetch IP information.")
+            return None
+    except Exception as e:
+        print("예상치 못한 오류가 발생했습니다, 개발자에게 문의해주세요.")
 
 def display_ip_info(ip_info):
     if ip_info:
         if 'ip' in ip_info:
             print(f"Your IP address: {ip_info['ip']}")
         if 'country_code' in ip_info:
-            print(f"Country Code: {ip_info['country_code']}")
-        if 'country_name' in ip_info:
-            print("Country Name:")
-            for lang, name in ip_info['country_name'].items():
-                print(f"  - {lang}: {name}")
+            print(f"국가 코드: {ip_info['country_code']}")
     else:
-        print("No IP information available.")
+        print("확인 가능한 IP 주소가 없습니다")
 
 # Fetch IP information and display it
-ipv4_info = get_ip_info(ip_version='ipv4', output_format='json')
-ipv6_info = get_ip_info(ip_version='ipv6', output_format='json')
+ipv4_info = get_ip_info(output_format='json')
 
-print("IPv4 Information:")
+print("IPv4 정보:")
 display_ip_info(ipv4_info)
 
-print("\nIPv6 Information:")
-display_ip_info(ipv6_info)
